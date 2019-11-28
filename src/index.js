aac356ee5003f3f24522306baebda02f98a707f6
ofaaoficial <oscarfamado@gmail.com>
2019-11-28 08:24:02 -0500

    configuration of babel and create a basic server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import '@babel/polyfill';
 import express from 'express';
 
-express();
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+async function main(){
+    await app.listen(app.get('port'));
+    console.log(`Server runnig in http://localhost:${app.get('port')}/`);
+}
+
+main();
