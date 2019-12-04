0d1d738b5ac881ea0acc47f09818b7a4e165f5d4
ofaaoficial <oscarfamado@gmail.com>
2019-12-03 22:17:55 -0500

    change code for create server and running

--- backend/src/controller/index.controller.js
+++ backend/src/controller/index.controller.js
@@ -1,15 +1,3 @@
-import Chat from '../models/Chat';
-
 export async function index(req, res){
-    const data = await Chat.find();
-    const messages = data.length > 0 ? data : [
-        {
-            message: "Message 1",
-        },
-        {
-            message: "Message 2",
-        },
-    ];
-
-    res.render('index/index',{ messages });
+    res.send('running');
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,39 +1,20 @@
 import '@babel/polyfill';
-import express, {static as st} from 'express';
-import indexRoutes from './router/index.router';
-import exphbs from 'express-handlebars';
-import path from 'path';
-import connect from './config/database';
+import express from 'express';
+import socketio from 'socket.io';
 import http from 'http';
-import socket from 'socket.io';
-
-connect();
+import indexRouter from './router/index.router';
 
 const app = express();
 const server = http.createServer(app);
-const io = socket(server);
-require('./config/socket.io')(io);
+const io = socketio(server);
 
+app.set('port', process.env.PORT || 5000);
 
-app.set('port', process.env.PORT || 2323);
-
-app.use(indexRoutes);
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(st(path.join(__dirname, 'public')));
-
-app.engine('hbs', exphbs({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs'
-}));
-
-app.set('view engine', 'hbs');
+app.use(indexRouter);
 
 async function main(){
     await server.listen(app.get('port'));
-    console.log(`Server running in http://localhost:${app.get('port')}/`);
+    console.log(`Server has started on http://localhost:${app.get('port')}/`);
 }
 
 main();
