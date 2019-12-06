e03054aae4f32a21f3943fd6303d52281eaf44c9
ofaaoficial <oscarfamado@gmail.com>
2019-12-05 22:57:05 -0500

    Input styles

--- frontend/src/components/Input/Input.js
+++ frontend/src/components/Input/Input.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 import './Input.css';
+import sendIcon from './send.png';
 
 const Input = ({message, setMessage, sendMessage}) => (
-    <form>
+    <form className="input-message">
         <input
             type="text"
             value={message}
             onChange={event => setMessage(event.target.value)}
             onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
             placeholder="Type a message..."
+            autoFocus
+            className="form-control"
         />
-        <button onClick={event => sendMessage(event)}>Send</button>
+        <button
+            className="btn btn-send-message"
+            onClick={event => sendMessage(event)}>
+            <img src={sendIcon} alt="Send icon"/>
+        </button>
     </form>
 );
 
