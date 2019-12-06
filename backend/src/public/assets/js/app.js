9ead7ee8e528319a8ab3329b0396efcfdce2a0f5
ofaaoficial <oscarfamado@gmail.com>
2019-12-05 21:11:54 -0500

    deliting incesary files of backend and adding general styles of application

--- backend/src/public/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const el = e => document.querySelector(e);
-const cl = c => console.log(c);
-
-const sendMessage = () => {
-    const message = el('#message').value;
-    socket.emit(message);
-};
