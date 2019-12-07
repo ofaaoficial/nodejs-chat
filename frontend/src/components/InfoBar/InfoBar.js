32dc97e81177bdc37e4710e2681c1e22255b28b7
ofaaoficial <oscarfamado@gmail.com>
2019-12-06 21:23:31 -0500

    getting better styles and adding link to go back login form

--- frontend/src/components/InfoBar/InfoBar.js
+++ frontend/src/components/InfoBar/InfoBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 import './InfoBar.css';
 
 const InfoBar = ({room}) => (
-    <section className="room-information">
+    <section className="InfoBar">
         <h1>{room}</h1>
+        <Link to={'/'} className="close">
+            &times;
+        </Link>
     </section>
 );
 
