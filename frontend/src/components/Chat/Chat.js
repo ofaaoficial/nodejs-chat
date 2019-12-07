79dcb02e0a4ff9e4bee8be61dfd7923728ec2019
ofaaoficial <oscarfamado@gmail.com>
2019-12-06 21:22:56 -0500

    getting better styles

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -4,6 +4,7 @@ import io from 'socket.io-client';
 import InfoBar from "../InfoBar/InfoBar";
 import Input from '../Input/Input';
 import Messages from '../Messages/Messages';
+import TextContainer from "../TextContainer/TextContainer";
 
 import './Chat.css';
 
@@ -14,6 +15,7 @@ const Chat = ({location}) => {
     const [room, setRoom] = useState('');
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState('');
+    const [users, setUsers] = useState([]);
 
     const ENDPOINT = 'localhost:5000';
 
@@ -25,21 +27,26 @@ const Chat = ({location}) => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', {name, room}, () => {
-
+        socket.emit('join', {name, room}, error => {
+            if(error)
+                alert(error)
         });
-
-        return () => {
-            socket.emit('disconnect');
-            socket.off();
-        }
 
     }, [ENDPOINT, location.search]);
 
     useEffect(() => {
         socket.on('message', message => {
             setMessages([...messages, message]);
-        })
+        });
+
+        socket.on('roomData', ({users}) => {
+            setUsers(users);
+        });
+
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
     }, [messages]);
 
     const sendMessage = event => {
@@ -60,7 +67,7 @@ const Chat = ({location}) => {
                 setMessage={setMessage} 
                 sendMessage={sendMessage}/>
 
-                {/*<TextContainer users={users}/>*/}
+            <TextContainer users={users}/>
         </section>
     )
 };
